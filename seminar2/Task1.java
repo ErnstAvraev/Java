63305770893e540cd6deace856630ded3ef94298
Ernst Avraev <avraev.ernst@yandex.ru>
2023-04-17 20:00:25 +0800

    tasks 1 and 2 completed, added class input

---
+++ seminar2/Task1.java
@@ -0,0 +1,38 @@
+package Java.seminar2;
+
+import java.util.Scanner;
+
+// Дано четное число N (>0) и символы c1 и c2. Написать метод, который 
+// вернет строку длины N, которая состоит из чередующихся символов c1 и c2, начиная с c1.
+
+// 6 a b
+// Вернуть: ababab
+
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Введите число N: ");
+        Scanner input = new Scanner(System.in);
+        int users_num = input.nextInt();
+        System.out.println("Введите первый символ: ");
+        String users_char_1 = input.next();
+        System.out.println("Введите второй символ: ");
+        String users_char_2 = input.next();
+        input.close();
+        System.out.println(String_Maker(users_char_1, users_char_2, users_num));
+    }
+
+    public static String String_Maker(String char_1, String char_2, int string_length) {
+        StringBuilder final_string = new StringBuilder();
+        
+        for (int i = 0; i < string_length/2; i++) {
+            final_string.append(char_1).append(char_2);  // можно добавлять подряд в одну строку
+            // final_string.append(char_1);
+            // final_string.append(char_2);
+        }
+        return final_string.toString();
+    }
+}
+
+
---
+++ seminar2/Task2.java
@@ -0,0 +1,44 @@
+package Java.seminar2;
+
+// stroka = "Привет"
+// stroka1 = new String
+
+// stroka1 = "Привет"
+
+// stroka == stroka1 -----> False
+
+// stroka.equals(stroka1) - сравнивать содержание строк
+// .charAt(i) - обратиться к символу строки по индексу
+
+// Напишите метод, который сжимает строку. 
+// Пример: вход - aaaabbbcddaaa
+// результат - a4b3cd2a3
+
+public class Task2 {
+    public static void main(String[] args) {
+        String users_string = input.Str("Введите строку: ");
+        System.out.println(cp866(users_string));
+    }
+
+    public static String cp866(String users_string) {
+        int counter = 1;
+        StringBuilder users_cp866 = new StringBuilder();
+        for (int i = 0; i < users_string.length() - 1; i++) {
+            if (users_string.charAt(i) == users_string.charAt(i + 1)) {
+                counter++;
+            } 
+            else {
+                users_cp866.append(users_string.charAt(i));
+                if (counter != 1){
+                    users_cp866.append(counter);
+                    counter = 1;
+                }
+            }
+        }
+        users_cp866.append(users_string.charAt(users_string.length() - 1));
+        if (counter > 1) {
+            users_cp866.append(counter);
+        }
+        return users_cp866.toString();
+    }
+}    
