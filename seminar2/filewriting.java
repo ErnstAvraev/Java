4cbdb7168270f9aa7b8639e3577a05c1298a178c
Ernst Avraev <avraev.ernst@yandex.ru>
2023-04-17 20:09:50 +0800

    initial

---
+++ seminar2/filewriting.java
@@ -0,0 +1,27 @@
+package Java.seminar2;
+import java.io.File;
+import java.io.FileWriter;
+
+
+public class filewriting {
+    public static void main(String[] args) {
+        Integer n = 10;
+        String text = "TEST";
+        String file_name = "1.txt";
+        File file = new File("1.txt");
+        try{
+            FileWriter writer = new FileWriter(file,false); // false - файл перезапичывается, true - дописывается
+            for (int i = 0; i < n; i++){
+                writer.write(text);
+                writer.write("\n");
+            }
+            writer.close();
+            System.out.println("Получилось!");
+        }
+        catch(Exception e){
+            System.out.println("Что то пошло не так");
+        }
+    }
+        
+}
+
