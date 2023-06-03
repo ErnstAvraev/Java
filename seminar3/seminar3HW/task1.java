51f1a3b457df48e4d9531d55f1c3279aca8c1391
Ernst Avraev <avraev.ernst@yandex.ru>
2023-06-03 20:02:58 +0800

    initial commit

---
+++ seminar3/seminar3HW/task1.java
@@ -0,0 +1,7 @@
+package seminar3.seminar3HW;
+// Пусть дан произвольный список целых чисел, удалить из него чётные числа
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
