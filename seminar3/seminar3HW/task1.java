d08f711a2edb0d112288fdc17cf2e63761fde3f6
Ernst Avraev <avraev.ernst@yandex.ru>
2023-06-03 20:14:56 +0800

    task1 completed

--- seminar3/seminar3HW/task1.java
+++ seminar3/seminar3HW/task1.java
@@ -1,7 +1,23 @@
 package seminar3.seminar3HW;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 // Пусть дан произвольный список целых чисел, удалить из него чётные числа
 public class task1 {
     public static void main(String[] args) {
-        
+        ArrayList <Integer> list = new ArrayList<Integer>();
+        Random rnd = new Random();
+        for (int i = 0; i < 20; i++) {
+            list.add(rnd.nextInt(1,11));
+        }
+        System.out.println(list);
+
+        for(int i = list.size()-1; i >= 0; i--) {
+            if (list.get(i)%2 ==0) {
+                list.remove(i);
+            }
+        }
+        System.out.println(list);
     }
 }
